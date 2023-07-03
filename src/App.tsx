b9d92bf91c88c709b2d64a23b93b8c56b47a5410
XiaKeMing <xiakeming97@gmail.com>
2023-07-03 10:00:29 +0800

    :rocket: chore(公共样式文件和button组件): 对项目的公共样式进行完善同时安装classnames插件与构建button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Button,{ButtonSize,ButtonType} from './components/Button/button'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button disabled>button</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>hello</Button>
+      <Button btnType={ButtonType.Link} href='www.baidu.com' disabled>百度</Button>
     </div>
   );
 }
 
 export default App;
+ 
